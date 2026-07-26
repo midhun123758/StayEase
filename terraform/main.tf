f168bb5f0a8734fa4be6d27952485506f0bc72d8
midhun123758 <midhunchandran230@gmail.com>
2026-07-26 20:15:11 +0530

    Fallback to local Postgres container on EC2 due to IAM restrictions

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,3 +95,82 @@ Name = "stayease-rds"
 output "rds_endpoint" {
 value = aws_db_instance.stayease_rds.address
 }
+
+resource "aws_security_group" "stayease_sg" {
+  name        = "stayease-security-group"
+  description = "StayEase security group"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "stayease-sg"
+  }
+}
+
+resource "aws_ecr_repository" "stayease_ecr" {
+  name                 = "stayease"
+  image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_instance" "stayease_server" {
+  ami                         = "ami-09a9858973b288bdd"
+  instance_type               = "t3.small"
+  key_name                    = "stayease-key"
+  subnet_id                   = "subnet-069895598b9456d9f"
+  vpc_security_group_ids      = [aws_security_group.stayease_sg.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "StayEaseServer"
+  }
+}
+
+resource "aws_eip" "stayease_eip" {
+  domain   = "vpc"
+  instance = aws_instance.stayease_server.id
+
+  tags = {
+    Name = "stayease-eip"
+  }
+}
